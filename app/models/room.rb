b63099371a2d983345f0d9150f708f09af3ae203
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-08-31 15:59:25 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,8 @@ class Room < ApplicationRecord
   #↓vol.2.5 １つのチャットルームには、２人のユーザーが存在。
   has_many :room_users
   has_many :users, through: :room_users
-  
+  has_many :messages
+
   validates :name, presence: true
   #↑vol2.6. チャットルームを新規作成する時にルーム名が存在している場合のみ作成可にする為の記述
 end
