cb2435defd1d52775e668afafada0abe4b7afa16
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-08-31 17:03:29 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 class Room < ApplicationRecord
   #↓vol.2.5 １つのチャットルームには、２人のユーザーが存在。
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
   #↑vol2.6. チャットルームを新規作成する時にルーム名が存在している場合のみ作成可にする為の記述
