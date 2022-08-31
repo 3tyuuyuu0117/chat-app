cbdd9fdf287516909d202ee85c89038b2b85bdc1
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-08-31 14:50:32 +0900

    チャット画面の編集

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@ class Room < ApplicationRecord
   #↓vol.2.5 １つのチャットルームには、２人のユーザーが存在。
   has_many :room_users
   has_many :users, through: :room_users
-
+  
   validates :name, presence: true
   #↑vol2.6. チャットルームを新規作成する時にルーム名が存在している場合のみ作成可にする為の記述
 end
